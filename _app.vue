3b11325ff010935aeecb0c57fe76acc4f184dbbf
Alexey3439 <gorrus39@mail.ru>
2025-02-02 18:29:18 +0300

    try deploy

---
+++ _app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { NuxtLayout } from '#components';
+
+
+</script>
+
+<template>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
+</template>
--- i18n.config.ts
+++ i18n.config.ts
@@ -1,12 +1,18 @@
 export default defineI18nConfig(() => ({
 	legacy: false,
-	locale: 'en',
+	// locale: "en",
 	messages: {
 		en: {
-			welcome: 'Welcome',
+			welcome: "Welcome",
 		},
 		fr: {
-			welcome: 'Bienvenue',
+			welcome: "Bienvenue",
+		},
+		ru: {
+			welcome: "Приветствие",
+		},
+		cn: {
+			welcome: "ПриветствиеПриветствиеПриветствиеПриветствиеПриветствие",
 		},
 	},
 }))
