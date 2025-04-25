d13000d96a0069f5233af4d8307c44cb9cec71f0
gorrus39 <gorrus100@gmail.com>
2025-04-25 11:04:03 +0200

    fix css

--- components/catalog/id/9-admins.vue
+++ components/catalog/id/9-admins.vue
@@ -8,10 +8,17 @@ defineProps<{
 
 <template>
   <CatalogIdTitleBlock :text="$t('catalog.id.admins')" />
-  <div class="x-mx flex flex-wrap justify-between gap-M-20 mb-M-20 md:gap-D-20 md:mb-D-80">
+  <div class="flex-container x-mx flex flex-wrap justify-between gap-M-20 mb-M-20 md:gap-D-30 md:mb-D-80">
     <CatalogIdAdminCard v-for="admin in item.admins" :admin="admin" />
   </div>
   <div class="x-mx flex mb-M-73 md:mb-D-80">
     <ChanksButtonGoUp class="ms-auto" />
   </div>
 </template>
+
+<style>
+.flex-container::after {
+  content: "";
+  flex: auto;
+}
+</style>
