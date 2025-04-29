7ab4be7c67a11ccc8bdadda132791b1a9e9acb92
gorrus39 <gorrus100@gmail.com>
2025-04-29 12:03:50 +0200

    fix: view modal admin on /catalog/id

--- components/catalog/id/admin-modal.vue
+++ components/catalog/id/admin-modal.vue
@@ -12,11 +12,14 @@ defineProps<{
       <NuxtImg class="absolute z-0 h-full w-full" src="images/default/catalog-item-img-short/background-white.png" />
 
       <div class="relative z-10 flex flex-col items-center text-black gap-D-20 me-D-20 ms-D-20 p-D-30">
-        <img class="z-10 rounded-full w-D-96 h-D-96" :src="getCatalogAdminImageUrl(admin.avatar_path)" />
+        <img
+          class="z-10 rounded-full w-M-40 h-M-40 md:w-D-96 md:h-D-96"
+          :src="getCatalogAdminImageUrl(admin.avatar_path)"
+        />
 
-        <p class="text-D-40">{{ admin.name }}</p>
+        <p class="text-M-16 md:text-D-40">{{ admin.name }}</p>
 
-        <p class="text-D-20">{{ admin.description }}</p>
+        <p class="hidden text-D-20 md:block">{{ admin.description }}</p>
 
         <UButton :to="admin.link" block :label="$t('catalog.id.admin.view_account')" color="black" />
       </div>
