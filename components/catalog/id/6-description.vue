b4bda0e8bc3092cc0122cba924d38299bbc012a4
gorrus39 <gorrus100@gmail.com>
2025-04-29 12:07:09 +0200

    min fix

--- components/catalog/id/6-description.vue
+++ components/catalog/id/6-description.vue
@@ -24,7 +24,7 @@ defineProps<{
 
     <div
       class="relative z-10 overflow-hidden mb-M-0 md:mb-D-111"
-      :class="[isExpanded ? 'max-h-[103vw]' : 'max-h-none']"
+      :class="[isExpanded ? 'max-h-none' : 'max-h-[103vw]']"
     >
       <div
         class="x-mx border-e-[2px] border-t-[2px] pt-M-10 pe-M-10 text-M-14 md:border-e-[.5px] md:border-t-[.5px] md:pt-D-20 md:pe-D-20 md:text-D-32"
